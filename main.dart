b4bc920d9a969d5b282858e137b5959068a7e7ae
Ahmed Gamal <101002059+AhmedGamal-29@users.noreply.github.com>
2022-09-05 19:52:00 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,14 +4,14 @@ import 'input_page.dart';
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
-  @override
+/*  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
-      home: InputPage(),
+      home: InputPage(),*/
     );
   }
 }
